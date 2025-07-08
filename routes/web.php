392cc1c62b1dd3771040e61947444ac49033ed65
sheikdaw <52328052+sheikdaw@users.noreply.github.com>
2025-07-08 09:34:49 +0530

    main

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Mail;
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
 Route::get('/test-mail', function () {
     Mail::raw('This is a test email from Laravel.', function ($message) {
         $message->to('sheikjob888@gmail.com')
